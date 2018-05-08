eee254e44f689e5475ade3aaaf1a062f010e5018
drighetto <drighetto@excellium-services.com>
2018-05-08 18:35:30 +0200

    Prepare samples to implements detection on Powerpoint file formats asked in issue 1

--- install-non-maven-deps.bat
+++ install-non-maven-deps.bat
@@ -3,5 +3,6 @@ set CDIR=%CD%
 cd src\non-maven-libs
 call mvn install:install-file -Dfile=aspose-cells-8.7.0.jar -DgroupId=aspose.com -DartifactId=aspose-cells -Dversion=8.7.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=aspose-words-16.1.0-jdk16.jar -DgroupId=aspose.com -DartifactId=aspose-words -Dversion=16.1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=aspose-slides-18.4-jdk16.jar -DgroupId=aspose.com -DartifactId=aspose-slides -Dversion=18.4.0 -Dpackaging=jar
 cd %CDIR%
 pause
