cde453f6165bea1ed3ea6c4506c4718848511006
Dominique RIGHETTO <dominique.righetto@gmail.com>
2018-05-10 11:43:24 +0200

    Add TracisCI build

---
+++ install-non-maven-deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd src/non-maven-libs
+mvn install:install-file -Dfile=aspose-cells-8.7.0.jar -DgroupId=aspose.com -DartifactId=aspose-cells -Dversion=8.7.0 -Dpackaging=jar
+mvn install:install-file -Dfile=aspose-words-16.1.0-jdk16.jar -DgroupId=aspose.com -DartifactId=aspose-words -Dversion=16.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=aspose-slides-18.4-jdk16.jar -DgroupId=aspose.com -DartifactId=aspose-slides -Dversion=18.4.0 -Dpackaging=jar
