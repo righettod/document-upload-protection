9c9a4c7e09d33c3b61dcdb70db4cc8968de96881
Dominique RIGHETTO <dominique.righetto@gmail.com>
2018-05-12 08:27:28 +0200

    Refactor the implementation of the image sanitizer to don't use IJ API anymore due to issue when run on non-gui enabled system.

--- src/main/java/eu/righettod/poc/sanitizer/ImageDocumentSanitizerImpl.java
+++ src/main/java/eu/righettod/poc/sanitizer/ImageDocumentSanitizerImpl.java
@@ -1,100 +1,173 @@
 package eu.righettod.poc.sanitizer;
 
-import ij.IJ;
-import ij.ImagePlus;
-import ij.io.Opener;
-import ij.process.ImageProcessor;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.ImageParser;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.formats.bmp.BmpImageParser;
+import org.apache.commons.imaging.formats.gif.GifImageParser;
+import org.apache.commons.imaging.formats.pcx.PcxImageParser;
+import org.apache.commons.imaging.formats.png.PngImageParser;
+import org.apache.commons.imaging.formats.tiff.TiffImageParser;
+import org.apache.commons.imaging.formats.wbmp.WbmpImageParser;
+import org.apache.commons.imaging.formats.xbm.XbmImageParser;
+import org.apache.commons.imaging.formats.xpm.XpmImageParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * Implementation of the sanitizer for Image file.
- * 
- * @see "http://stackoverflow.com/a/24747085"
+ * <p>
+ * Use Java built-in API in complement of Apache Commons Imaging for format not supported by the built-in API.
  *
+ * @see "http://commons.apache.org/proper/commons-imaging/"
+ * @see "http://commons.apache.org/proper/commons-imaging/formatsupport.html"
  */
 public class ImageDocumentSanitizerImpl implements DocumentSanitizer {
 
-	/** LOGGER */
-	private static final Logger LOG = LoggerFactory.getLogger(ImageDocumentSanitizerImpl.class);
+    /**
+     * LOGGER
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ImageDocumentSanitizerImpl.class);
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see eu.righettod.poc.sanitizer.DocumentSanitizer#madeSafe(java.io.File)
-	 */
-	@Override
-	public boolean madeSafe(File f) {
-		boolean safeState = false;
-		try {
-			if ((f != null) && f.exists() && f.canRead() && f.canWrite()) {
-				// Load image
-				ImagePlus image = new Opener().openImage(f.getAbsolutePath());
 
-				// Check that image has been successfully loaded
-				if (image == null) {
-					throw new Exception("Cannot load the original image !");
-				}
+    /**
+     * {@inheritDoc}
+     *
+     * @see eu.righettod.poc.sanitizer.DocumentSanitizer#madeSafe(java.io.File)
+     */
+    @Override
+    public boolean madeSafe(File f) {
+        boolean safeState = false;
+        boolean fallbackOnApacheCommonsImaging;
+        try {
+            if ((f != null) && f.exists() && f.canRead() && f.canWrite()) {
+                //Get the image format
+                String formatName;
+                try (ImageInputStream iis = ImageIO.createImageInputStream(f)) {
+                    Iterator<ImageReader> imageReaderIterator = ImageIO.getImageReaders(iis);
+                    //If there not ImageReader instance found so it's means that the current format is not supported by the Java built-in API
+                    if (!imageReaderIterator.hasNext()) {
+                        ImageInfo imageInfo = Imaging.getImageInfo(f);
+                        if (imageInfo != null && imageInfo.getFormat() != null && imageInfo.getFormat().getName() != null) {
+                            formatName = imageInfo.getFormat().getName();
+                            fallbackOnApacheCommonsImaging = true;
+                        } else {
+                            throw new IOException("Format of the original image is not supported for read operation !");
+                        }
+                    } else {
+                        ImageReader reader = imageReaderIterator.next();
+                        formatName = reader.getFormatName();
+                        fallbackOnApacheCommonsImaging = false;
+                    }
+                }
 
-				// Get current Width and Height of the image
-				int originalWidth = image.getWidth();
-				int originalHeight = image.getHeight();
+                // Load the image
+                BufferedImage originalImage;
+                if (!fallbackOnApacheCommonsImaging) {
+                    originalImage = ImageIO.read(f);
+                } else {
+                    originalImage = Imaging.getBufferedImage(f);
+                }
 
-				// Obtain an Image processor on this image
-				ImageProcessor originalImageProcessor = image.getProcessor();
-				if (originalImageProcessor == null) {
-					throw new Exception("Cannot obtains an image processor for the original image !");
-				}
+                // Check that image has been successfully loaded
+                if (originalImage == null) {
+                    throw new IOException("Cannot load the original image !");
+                }
 
-				// Resize the image by removing 1px on Width and Height
-				ImageProcessor resizedImageProcessor = originalImageProcessor.resize(originalWidth - 1, originalHeight - 1);
-				if (resizedImageProcessor == null) {
-					throw new Exception("Cannot resize the original image !");
-				}
+                // Get current Width and Height of the image
+                int originalWidth = originalImage.getWidth(null);
+                int originalHeight = originalImage.getHeight(null);
 
-				// Resize the resized image by adding 1px on Width and Height
-				// In fact set image to is initial size
-				ImageProcessor initialSizedImageProcessor = resizedImageProcessor.resize(originalWidth, originalHeight);
-				if (initialSizedImageProcessor == null) {
-					throw new Exception("Cannot restore the initial size of the original image !");
-				}
 
-				// Save image and detect the image format for provided file
-				String imageFormat = Opener.getFileFormat(f.getAbsolutePath());
-				ImagePlus finalImg = new ImagePlus("", initialSizedImageProcessor);
-				IJ.saveAs(finalImg, imageFormat, f.getAbsolutePath());
+                // Resize the image by removing 1px on Width and Height
+                Image resizedImage = originalImage.getScaledInstance(originalWidth - 1, originalHeight - 1, Image.SCALE_SMOOTH);
 
-				// IJ will save the file with the extension associated to the image format (ex: jpg or png)
-				// but, as the provided input file can have any extension (we do not use it to detect image format),
-				// then we must manage the case in which 2 files exists at this point:
-				// 1) The input file provided (ex: myfile.tmp)
-				// 2) The new file saved by IJ (ex: myfile.png)
-				String tmp = f.getName();
-				String newSavedFileName = tmp.substring(0, tmp.lastIndexOf(".") + 1) + imageFormat;
-				File newSavedFile = new File(f.getParentFile(), newSavedFileName);
-				if (newSavedFile.exists() && !f.getAbsolutePath().equalsIgnoreCase(newSavedFile.getAbsolutePath())) {
-					// Overwrite content of the input file with the content of the new saved file
-					Files.copy(newSavedFile.toPath(), f.toPath(), StandardCopyOption.REPLACE_EXISTING);
-					// Remove file saved by IJ
-					newSavedFile.delete();
-				}
+                // Resize the resized image by adding 1px on Width and Height - In fact set image to is initial size
+                Image initialSizedImage = resizedImage.getScaledInstance(originalWidth, originalHeight, Image.SCALE_SMOOTH);
 
-				// Set state flag
-				safeState = true;
-			}
+                // Save image by overwriting the provided source file content
+                BufferedImage sanitizedImage = new BufferedImage(initialSizedImage.getWidth(null), initialSizedImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+                Graphics bg = sanitizedImage.getGraphics();
+                bg.drawImage(initialSizedImage, 0, 0, null);
+                bg.dispose();
+                try (OutputStream fos = Files.newOutputStream(f.toPath(), StandardOpenOption.WRITE)) {
+                    if (!fallbackOnApacheCommonsImaging) {
+                        ImageIO.write(sanitizedImage, formatName, fos);
+                    } else {
+                        ImageParser imageParser;
+                        //Handle only formats for which Apache Commons Imaging can successfully write (YES in Write column of the reference link) the image format
+                        //See reference link in the class header
+                        switch (formatName) {
+                            case "TIFF": {
+                                imageParser = new TiffImageParser();
+                                break;
+                            }
+                            case "PCX": {
+                                imageParser = new PcxImageParser();
+                                break;
+                            }
+                            case "DCX": {
+                                imageParser = new PcxImageParser();
+                                break;
+                            }
+                            case "BMP": {
+                                imageParser = new BmpImageParser();
+                                break;
+                            }
+                            case "GIF": {
+                                imageParser = new GifImageParser();
+                                break;
+                            }
+                            case "PNG": {
+                                imageParser = new PngImageParser();
+                                break;
+                            }
+                            case "WBMP": {
+                                imageParser = new WbmpImageParser();
+                                break;
+                            }
+                            case "XBM": {
+                                imageParser = new XbmImageParser();
+                                break;
+                            }
+                            case "XPM": {
+                                imageParser = new XpmImageParser();
+                                break;
+                            }
+                            default: {
+                                throw new IOException("Format of the original image is not supported for write operation !");
+                            }
 
-		}
-		catch (Exception e) {
-			safeState = false;
-			LOG.warn("Error during Image file processing !", e);
-		}
+                        }
+                        imageParser.writeImage(sanitizedImage, fos, new HashMap<>());
+                    }
 
-		return safeState;
-	}
+                }
+
+                // Set state flag
+                safeState = true;
+            }
+
+        } catch (Exception e) {
+            safeState = false;
+            LOG.warn("Error during Image file processing !", e);
+        }
+
+        return safeState;
+    }
 
 }
--- src/test/java/eu/righettod/poc/sanitizer/ImageDocumentSanitizerImplTest.java
+++ src/test/java/eu/righettod/poc/sanitizer/ImageDocumentSanitizerImplTest.java
@@ -1,156 +1,178 @@
 package eu.righettod.poc.sanitizer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Locale;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
+import org.apache.commons.imaging.ImageFormats;
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.formats.gif.GifImageParser;
+import org.apache.commons.imaging.formats.jpeg.JpegImageParser;
+import org.apache.commons.imaging.formats.tiff.TiffImageParser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Locale;
 
 /**
  * Test cases for image sanitizer implementation.<br>
  * Use ".tmp" extension for working sample in order to ensure that the image API do not use the file extension to detect image format.
- *
  */
-@SuppressWarnings("static-method")
 public class ImageDocumentSanitizerImplTest {
 
-	/** Home directory of the Images samples */
-	private static final File SAMPLES_DIRECTORY = new File("src/test/resources/image");
+    /**
+     * Home directory of the Images samples
+     */
+    private static final File SAMPLES_DIRECTORY = new File("src/test/resources/image");
 
-	/** Working directory */
-	private static final File WORKING_DIRECTORY = new File("target/work");
+    /**
+     * Working directory
+     */
+    private static final File WORKING_DIRECTORY = new File("target/work");
 
-	/** Tested class instance */
-	private DocumentSanitizer victim = new ImageDocumentSanitizerImpl();
+    /**
+     * Tested class instance
+     */
+    private DocumentSanitizer victim = new ImageDocumentSanitizerImpl();
 
-	/**
-	 * Initialize workspace before a test case execution
-	 * 
-	 * @throws IOException
-	 */
-	@Before
-	public void initTest() throws IOException {
-		if (WORKING_DIRECTORY.exists()) {
-			WORKING_DIRECTORY.delete();
-		}
-		WORKING_DIRECTORY.mkdirs();
-	}
+    /**
+     * Initialize workspace before a test case execution
+     */
+    @Before
+    public void initTest() {
+        if (WORKING_DIRECTORY.exists()) {
+            WORKING_DIRECTORY.delete();
+        }
+        WORKING_DIRECTORY.mkdirs();
+    }
 
-	/**
-	 * Test case for executable renamed as image<br>
-	 * Here program cannot make it safe.
-	 * 
-	 * @throws IOException
-	 * 
-	 */
-	@Test
-	public void testExeRenamedAsImg() throws IOException {
-		// Prepare test
-		File sample = new File(SAMPLES_DIRECTORY, "test-exe-renamed-as-img.png");
-		File workingSample = new File(WORKING_DIRECTORY, "test-exe-renamed-as-img.tmp");
-		Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		// Run test
-		boolean safeState = this.victim.madeSafe(workingSample);
-		// Validate test
-		Assert.assertFalse(safeState);
-	}
+    /**
+     * Test case for executable renamed as image<br>
+     * Here program cannot make it safe.
+     *
+     * @throws IOException If any error occurs
+     */
+    @Test
+    public void testExeRenamedAsImg() throws IOException {
+        // Prepare test
+        File sample = new File(SAMPLES_DIRECTORY, "test-exe-renamed-as-img.png");
+        File workingSample = new File(WORKING_DIRECTORY, "test-exe-renamed-as-img.tmp");
+        Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        // Run test
+        boolean safeState = this.victim.madeSafe(workingSample);
+        // Validate test
+        Assert.assertFalse(safeState);
+    }
 
-	/**
-	 * Test case for JPEG image containing code into one of his EXIF tags.<br>
-	 * Here program must successfully make it safe.
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	@Test
-	public void testJpegWithCodeInEXIFTag() throws Exception {
-		// Prepare test
-		File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-exif.jpg");
-		File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-exif.tmp");
-		Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		Assert.assertTrue(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
-		// Run test
-		boolean safeState = this.victim.madeSafe(workingSample);
-		// Validate test
-		Assert.assertFalse(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
-		Assert.assertTrue(safeState);
-	}
+    /**
+     * Test case for JPEG image containing code into one of his EXIF tags.<br>
+     * Here program must successfully make it safe.
+     *
+     * @throws Exception If any error occurs
+     */
+    @Test
+    public void testJpegWithCodeInEXIFTag() throws Exception {
+        // Prepare test
+        File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-exif.jpg");
+        File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-exif.tmp");
+        Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        Assert.assertTrue(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
+        // Run test
+        boolean safeState = this.victim.madeSafe(workingSample);
+        // Validate test
+        Assert.assertFalse(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
+        Assert.assertTrue(safeState);
+        //Ensure that the final file is still a valid image in the original image format
+        ImageInfo imageInfo = Imaging.getImageInfo(workingSample);
+        BufferedImage sanitizedImage = new JpegImageParser().getBufferedImage(Files.readAllBytes(workingSample.toPath()),new HashMap<>());
+        Assert.assertNotNull(imageInfo);
+        Assert.assertNotNull(sanitizedImage);
+        Assert.assertEquals(ImageFormats.JPEG.getName(),imageInfo.getFormat().getName());
+    }
 
-	/**
-	 * Test case for TIFF image containing code into one of his EXIF tags.<br>
-	 * Here program must successfully make it safe.
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	@Test
-	public void testTiffWithCodeInEXIFTag() throws Exception {
-		// Prepare test
-		File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-exif.tif");
-		File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-exif.tmp");
-		Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		Assert.assertTrue(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
-		// Run test
-		boolean safeState = this.victim.madeSafe(workingSample);
-		// Validate test
-		Assert.assertFalse(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
-		Assert.assertTrue(safeState);
-	}
+    /**
+     * Test case for TIFF image containing code into one of his EXIF tags.<br>
+     * Here program must successfully make it safe.
+     *
+     * @throws Exception If any error occurs
+     */
+    @Test
+    public void testTiffWithCodeInEXIFTag() throws Exception {
+        // Prepare test
+        File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-exif.tif");
+        File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-exif.tmp");
+        Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        Assert.assertTrue(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
+        // Run test
+        boolean safeState = this.victim.madeSafe(workingSample);
+        // Validate test
+        Assert.assertFalse(this.containsStringInEXIFTags(workingSample, "phpinfo()"));
+        Assert.assertTrue(safeState);
+        //Ensure that the final file is still a valid image in the original image format
+        ImageInfo imageInfo = Imaging.getImageInfo(workingSample);
+        BufferedImage sanitizedImage = new TiffImageParser().getBufferedImage(Files.readAllBytes(workingSample.toPath()),new HashMap<>());
+        Assert.assertNotNull(imageInfo);
+        Assert.assertNotNull(sanitizedImage);
+        Assert.assertEquals(ImageFormats.TIFF.getName(),imageInfo.getFormat().getName());
+    }
 
-	/**
-	 * Test case for GIF image containing code into one his content.<br>
-	 * Here program must successfully make it safe.
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	@Test
-	public void testGifWithCodeInContent() throws Exception {
-		// Prepare test
-		File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-img.gif");
-		File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-img.tmp");
-		Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		String sampleContent = new String(Files.readAllBytes(workingSample.toPath())).toLowerCase(Locale.US);
-		Assert.assertTrue(sampleContent.contains("phpinfo()"));
-		// Run test
-		boolean safeState = this.victim.madeSafe(workingSample);
-		// Validate test
-		sampleContent = new String(Files.readAllBytes(workingSample.toPath())).toLowerCase(Locale.US);
-		Assert.assertFalse(sampleContent.contains("phpinfo()"));
-		Assert.assertTrue(safeState);
-	}
+    /**
+     * Test case for GIF image containing code into one his content.<br>
+     * Here program must successfully make it safe.
+     *
+     * @throws Exception If any error occurs
+     */
+    @Test
+    public void testGifWithCodeInContent() throws Exception {
+        // Prepare test
+        File sample = new File(SAMPLES_DIRECTORY, "test-php-inserted-into-img.gif");
+        File workingSample = new File(WORKING_DIRECTORY, "test-php-inserted-into-img.tmp");
+        Files.copy(sample.toPath(), workingSample.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        String sampleContent = new String(Files.readAllBytes(workingSample.toPath())).toLowerCase(Locale.US);
+        Assert.assertTrue(sampleContent.contains("phpinfo()"));
+        // Run test
+        boolean safeState = this.victim.madeSafe(workingSample);
+        // Validate test
+        sampleContent = new String(Files.readAllBytes(workingSample.toPath())).toLowerCase(Locale.US);
+        Assert.assertFalse(sampleContent.contains("phpinfo()"));
+        Assert.assertTrue(safeState);
+        //Ensure that the final file is still a valid image in the original image format
+        ImageInfo imageInfo = Imaging.getImageInfo(workingSample);
+        BufferedImage sanitizedImage = new GifImageParser().getBufferedImage(Files.readAllBytes(workingSample.toPath()),new HashMap<>());
+        Assert.assertNotNull(imageInfo);
+        Assert.assertNotNull(sanitizedImage);
+        Assert.assertEquals(ImageFormats.GIF.getName(),imageInfo.getFormat().getName());
+    }
 
-	/**
-	 * Utility method to search a string into all the EXIF tags of an image file.
-	 * 
-	 * @param image Source image file
-	 * @param searched String searched
-	 * @return TRUE only the string is present
-	 * @throws Exception
-	 */
-	@SuppressWarnings("null")
-	private boolean containsStringInEXIFTags(File image, String searched) throws Exception {
-		boolean isFound = false;
-		Metadata metadata = ImageMetadataReader.readMetadata(image);
-		String tagDesc = null;
-		for (Directory dir : metadata.getDirectories()) {
-			for (Tag tag : dir.getTags()) {
-				tagDesc = (tag.getDescription() == null) ? "" : tag.getDescription().toLowerCase(Locale.US);
-				if (tagDesc.contains(searched.toLowerCase(Locale.US))) {
-					isFound = true;
-					break;
-				}
-			}
-		}
-		return isFound;
-	}
+    /**
+     * Utility method to search a string into all the EXIF tags of an image file.
+     *
+     * @param image    Source image file
+     * @param searched String searched
+     * @return TRUE only the string is present
+     * @throws Exception If any error occurs
+     */
+    private boolean containsStringInEXIFTags(File image, String searched) throws Exception {
+        boolean isFound = false;
+        Metadata metadata = ImageMetadataReader.readMetadata(image);
+        String tagDesc;
+        for (Directory dir : metadata.getDirectories()) {
+            for (Tag tag : dir.getTags()) {
+                tagDesc = (tag.getDescription() == null) ? "" : tag.getDescription().toLowerCase(Locale.US);
+                if (tagDesc.contains(searched.toLowerCase(Locale.US))) {
+                    isFound = true;
+                    break;
+                }
+            }
+        }
+        return isFound;
+    }
 }
